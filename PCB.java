d3412864f77280dd7b58185456a6eca0b768833b
enenery <erii.sugimoto@stu.bmcc.cuny.edu>
2017-04-17 11:04:44 -0400

    Add existing file

---
+++ PCB.java
@@ -0,0 +1,31 @@
+public class PCB{
+
+private int jobNumber;
+private int priority;
+private int jobSize;
+private int maxCPUTime;
+private int submissionTime;
+
+public PCB(int jobNumber, int priority, int jobSize, int maxCPUTime, int submissionTime){
+    this.jobNumber = jobNumber;
+    this.priority = priority;
+    this.jobSize = jobSize;
+    this.maxCPUTime = maxCPUTime;
+    this.submissionTime = submissionTime;
+}
+
+//accesors for fields
+public int getJobNumber(){return jobNumber;}
+public int getPriority(){return priority;}
+public int getJobSize(){return jobSize;}
+public int getMaxCPUTime(){return maxCPUTime;}
+public int getSubmissionTime(){return submissionTime;}
+
+//mutators for fields
+public void setJobNumber(int jobNumber){this.jobNumber = jobNumber;}
+public void setPriority(int priority){this.priority = priority;}
+public void setJobSize(int jobSize){this.jobSize = jobSize;}
+public void setMaxCPUTime(int maxCPUTime){this.maxCPUTime = maxCPUTime;}
+public void setSubmissionTime(int submissionTime){this.submissionTime = submissionTime;}
+
+}
